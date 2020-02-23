3ba23ece5af0a3db2b84f303e843820ce99cc4f9
teddykwon <teddy.kwon89@gmail.com>
2020-02-23 19:41:39 +0900

    추가 test commit

---
+++ _next/static/webpack/static/development/pages/_error.js.9dca131291f8aa7870a8.hot-update.js
@@ -0,0 +1,163 @@
+webpackHotUpdate("static/development/pages/_error.js",{
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/array/from.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/set.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/arrayWithoutHoles.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/iterableToArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/nonIterableSpread.js":
+false,
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/toConsumableArray.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/array/from.js":
+false,
+
+/***/ "./node_modules/core-js/library/fn/set.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/_create-property.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es6.array.from.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es6.set.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es7.set.from.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es7.set.of.js":
+false,
+
+/***/ "./node_modules/core-js/library/modules/es7.set.to-json.js":
+false,
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=%2FUsers%2Fdalbang%2FDevelop%2Fgit%2Fkoko%2Ffront%2Fpages%2F_error%2Findex.tsx!./":
+/*!*************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=%2FUsers%2Fdalbang%2FDevelop%2Fgit%2Fkoko%2Ffront%2Fpages%2F_error%2Findex.tsx ***!
+  \*************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/_error", function() {
+      var mod = __webpack_require__(/*! ./pages/_error/index.tsx */ "./pages/_error/index.tsx")
+      if(true) {
+        module.hot.accept(/*! ./pages/_error/index.tsx */ "./pages/_error/index.tsx", function() {
+          if(!next.router.components["/_error"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/_error/index.tsx */ "./pages/_error/index.tsx")
+          next.router.update("/_error", updatedPage)
+        })
+      }
+      return mod
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=next%2Fdist%2Fpages%2F_error!./":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/amp-context.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/amp.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/head-manager-context.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/head.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/side-effect.js":
+false,
+
+/***/ "./node_modules/next/dist/pages/_error.js":
+false,
+
+/***/ "./pages/_error/index.tsx":
+/*!********************************!*\
+  !*** ./pages/_error/index.tsx ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/regenerator */ "./node_modules/@babel/runtime-corejs2/regenerator/index.js");
+/* harmony import */ var _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _server_i18n__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../server/i18n */ "./server/i18n.ts");
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1__["createElement"];
+// #region Global Imports
+
+// #endregion Global Imports
+// #region Local Imports
+ // #endregion Local Imports
+// #region Interface Imports
+
+// #endregion Interface Imports
+var Error = function Error(_ref) {
+  var t = _ref.t,
+      statusCode = _ref.statusCode;
+  return __jsx("div", null, t("common:Error"), statusCode);
+};
+
+Error.getInitialProps = function _callee(_ref2) {
+  var res, err, statusCode;
+  return _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          res = _ref2.res, err = _ref2.err;
+
+          if (res) {
+            statusCode = res.statusCode;
+          } else if (err) {
+            statusCode = err.statusCode;
+          }
+
+          return _context.abrupt("return", {
+            namespacesRequired: ["common"],
+            statusCode: statusCode
+          });
+
+        case 3:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Object(_server_i18n__WEBPACK_IMPORTED_MODULE_2__["withTranslation"])("common")(Error));
+
+/***/ }),
+
+/***/ 4:
+/*!*****************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F_error&absolutePagePath=%2FUsers%2Fdalbang%2FDevelop%2Fgit%2Fkoko%2Ffront%2Fpages%2F_error%2Findex.tsx ***!
+  \*****************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F_error&absolutePagePath=%2FUsers%2Fdalbang%2FDevelop%2Fgit%2Fkoko%2Ffront%2Fpages%2F_error%2Findex.tsx! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=%2FUsers%2Fdalbang%2FDevelop%2Fgit%2Fkoko%2Ffront%2Fpages%2F_error%2Findex.tsx!./");
+
+
+/***/ })
+
+})
+//# sourceMappingURL=_error.js.9dca131291f8aa7870a8.hot-update.js.map
---
+++ _next/static/webpack/static/development/pages/home.js.f0d21dd1ec3320695984.hot-update.js
@@ -0,0 +1,168 @@
+webpackHotUpdate("static/development/pages/home.js",{
+
+/***/ "./pages/home/index.tsx":
+/*!******************************!*\
+  !*** ./pages/home/index.tsx ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/regenerator */ "./node_modules/@babel/runtime-corejs2/regenerator/index.js");
+/* harmony import */ var _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
+/* harmony import */ var _server_i18n__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../server/i18n */ "./server/i18n.ts");
+/* harmony import */ var _src_Styled_Home__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../src/Styled/Home */ "./src/Styled/Home.ts");
+/* harmony import */ var _src_Actions__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../../src/Actions */ "./src/Actions/index.ts");
+/* harmony import */ var _src_Components__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../../src/Components */ "./src/Components/index.ts");
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1__["createElement"];
+// #region Global Imports
+
+ // #endregion Global Imports
+// #region Local Imports
+
+
+
+
+ // #endregion Local Imports
+// #region Interface Imports
+
+// #endregion Interface Imports
+var Home = function Home(_ref) {
+  var t = _ref.t,
+      i18n = _ref.i18n;
+  var home = Object(react_redux__WEBPACK_IMPORTED_MODULE_2__["useSelector"])(function (state) {
+    return state.home;
+  });
+  var dispatch = Object(react_redux__WEBPACK_IMPORTED_MODULE_2__["useDispatch"])();
+
+  var renderHashTagButtons = function renderHashTagButtons(activeTag) {
+    return ["puppy", "dog"].map(function (tag) {
+      return __jsx(_src_Components__WEBPACK_IMPORTED_MODULE_6__["HashTagButton"], {
+        key: tag,
+        tag: tag,
+        isActive: activeTag === tag,
+        onClick: function onClick() {
+          return alert("");
+        }
+      });
+    });
+  };
+
+  var renderFeeds = function renderFeeds() {
+    return [{
+      vendorType: "TIKTOK",
+      tags: ["댕댕이", "강아지"],
+      title: "#포메라니안 #puppylove #doglife #doga #puppet #dost #반려동물 #고양이 #마이펫 #dogtricks #강아지 #puppy #puppydog #dogs #doggo #dog #멍멍이 #pugpuppy #아기강아지",
+      content: {
+        type: "VIDEO",
+        thumbnailImageUrl: "https://p16-tiktokcdn-com.akamaized.net/obj/v0201/869f1e8b47839774413cd9e7b16194f2",
+        link: "https://www.tiktok.com/@dogcastle1/video/6787215908818390274",
+        originUrl: null
+      },
+      author: {
+        name: "DOGCASTLE",
+        profileImageUrl: null
+      }
+    }, {
+      vendorType: "TIKTOK",
+      tags: ["댕댕이", "강아지"],
+      title: "#포메라니안 #puppylove #doglife #doga #puppet #dost #반려동물 #고양이 #마이펫 #dogtricks #강아지 #puppy #puppydog #dogs #doggo #dog #멍멍이 #pugpuppy #아기강아지",
+      content: {
+        type: "VIDEO",
+        thumbnailImageUrl: "https://p16-tiktokcdn-com.akamaized.net/obj/v0201/869f1e8b47839774413cd9e7b16194f2",
+        link: "https://www.tiktok.com/@dogcastle1/video/6787215908818390274",
+        originUrl: null
+      },
+      author: {
+        name: "DOGCASTLE",
+        profileImageUrl: null
+      }
+    }, {
+      vendorType: "TIKTOK",
+      tags: ["댕댕이", "강아지"],
+      title: "#포메라니안 #puppylove #doglife #doga #puppet #dost #반려동물 #고양이 #마이펫 #dogtricks #강아지 #puppy #puppydog #dogs #doggo #dog #멍멍이 #pugpuppy #아기강아지",
+      content: {
+        type: "VIDEO",
+        thumbnailImageUrl: "https://p16-tiktokcdn-com.akamaized.net/obj/v0201/869f1e8b47839774413cd9e7b16194f2",
+        link: "https://www.tiktok.com/@dogcastle1/video/6787215908818390274",
+        originUrl: null
+      },
+      author: {
+        name: "DOGCASTLE",
+        profileImageUrl: null
+      }
+    }, {
+      vendorType: "TIKTOK",
+      tags: ["댕댕이", "강아지"],
+      title: "#포메라니안 #puppylove #doglife #doga #puppet #dost #반려동물 #고양이 #마이펫 #dogtricks #강아지 #puppy #puppydog #dogs #doggo #dog #멍멍이 #pugpuppy #아기강아지",
+      content: {
+        type: "VIDEO",
+        thumbnailImageUrl: "https://p16-tiktokcdn-com.akamaized.net/obj/v0201/869f1e8b47839774413cd9e7b16194f2",
+        link: "https://www.tiktok.com/@dogcastle1/video/6787215908818390274",
+        originUrl: null
+      },
+      author: {
+        name: "DOGCASTLE",
+        profileImageUrl: null
+      }
+    }].map(function (feed, i) {
+      return __jsx(_src_Components__WEBPACK_IMPORTED_MODULE_6__["Feed"], {
+        key: i,
+        vendorType: feed.vendorType,
+        tags: feed.tags,
+        title: feed.title,
+        postedAt: "2020.02.02",
+        author: {
+          name: feed.author.name,
+          profileImgUrl: feed.author.profileImageUrl
+        },
+        content: {
+          thumbnailUrl: feed.content.thumbnailImageUrl,
+          originUrl: feed.content.originUrl,
+          link: feed.content.link,
+          type: feed.content.type
+        }
+      });
+    });
+  };
+
+  return __jsx(_src_Styled_Home__WEBPACK_IMPORTED_MODULE_4__["Container"], null, __jsx(_src_Styled_Home__WEBPACK_IMPORTED_MODULE_4__["Top"], null), __jsx(_src_Styled_Home__WEBPACK_IMPORTED_MODULE_4__["Middle"], null, __jsx(_src_Styled_Home__WEBPACK_IMPORTED_MODULE_4__["MiddleLeft"], null, __jsx(_src_Styled_Home__WEBPACK_IMPORTED_MODULE_4__["MiddleLeftButtons"], null, renderHashTagButtons("puppy"))), __jsx(_src_Styled_Home__WEBPACK_IMPORTED_MODULE_4__["MiddleRight"], null, __jsx(_src_Styled_Home__WEBPACK_IMPORTED_MODULE_4__["TopText"], null, "#puppy"), __jsx(_src_Styled_Home__WEBPACK_IMPORTED_MODULE_4__["FeedBoxs"], null, renderFeeds()))));
+};
+
+Home.getInitialProps = function _callee(ctx) {
+  return _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.awrap(ctx.store.dispatch(_src_Actions__WEBPACK_IMPORTED_MODULE_5__["HomeActions"].GetApod({
+            params: {
+              hd: true
+            }
+          })));
+
+        case 2:
+          return _context.abrupt("return", {
+            namespacesRequired: ["common"]
+          });
+
+        case 3:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+};
+
+var Extended = Object(_server_i18n__WEBPACK_IMPORTED_MODULE_3__["withTranslation"])("common")(Home);
+/* harmony default export */ __webpack_exports__["default"] = (Extended);
+
+/***/ })
+
+})
+//# sourceMappingURL=home.js.f0d21dd1ec3320695984.hot-update.js.map
