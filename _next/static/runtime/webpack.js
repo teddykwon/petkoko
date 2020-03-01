b3e3c999eb6c8b7acbbb038a62b51fef2dfe65e8
teddykwon <teddy.kwon89@gmail.com>
2020-03-01 14:57:19 +0900

    grid gap up

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c2861f33e725fca70885";
+/******/ 	var hotCurrentHash = "403535f23ea2daa8c66a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/static/development/pages/index.js.c2861f33e725fca70885.hot-update.js
@@ -0,0 +1,63 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./src/Styled/Home.ts":
+/*!****************************!*\
+  !*** ./src/Styled/Home.ts ***!
+  \****************************/
+/*! exports provided: Container, Top, Middle, MiddleLeft, MiddleLeftButtons, MiddleRight, FeedBoxs, TopText */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Container", function() { return Container; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Top", function() { return Top; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Middle", function() { return Middle; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MiddleLeft", function() { return MiddleLeft; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MiddleLeftButtons", function() { return MiddleLeftButtons; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "MiddleRight", function() { return MiddleRight; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "FeedBoxs", function() { return FeedBoxs; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "TopText", function() { return TopText; });
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+// #region Global Imports
+ // #endregion Global Imports
+
+var Container = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].div.withConfig({
+  displayName: "Home__Container",
+  componentId: "sc-1vgusud-0"
+})(["font-family:Muli,Arial,Helvetica,sans-serif;background-color:", ";display:flex;flex-direction:column;flex:1 1 100%;justify-content:flex-start;align-items:center;min-height:100vh;"], function (_ref) {
+  var theme = _ref.theme;
+  return theme.colors.primary;
+});
+var Top = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].div.withConfig({
+  displayName: "Home__Top",
+  componentId: "sc-1vgusud-1"
+})(["margin-top:100px;"]);
+var Middle = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].div.withConfig({
+  displayName: "Home__Middle",
+  componentId: "sc-1vgusud-2"
+})(["padding:0 30px;display:flex;flex:1 1 100%;width:100%;justify-content:flex-start;"]);
+var MiddleLeft = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].div.withConfig({
+  displayName: "Home__MiddleLeft",
+  componentId: "sc-1vgusud-3"
+})(["display:flex;flex:1 1 240px;"]);
+var MiddleLeftButtons = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].div.withConfig({
+  displayName: "Home__MiddleLeftButtons",
+  componentId: "sc-1vgusud-4"
+})(["font-weight:700;font-size:20px;color:#000000;letter-spacing:normal;display:flex;flex-direction:column;.active{background-color:#536dfe;color:#ffffff;}"]);
+var MiddleRight = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].div.withConfig({
+  displayName: "Home__MiddleRight",
+  componentId: "sc-1vgusud-5"
+})(["display:flex;flex-direction:column;flex:1 1 100%;margin:0px 40px;"]);
+var FeedBoxs = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].div.withConfig({
+  displayName: "Home__FeedBoxs",
+  componentId: "sc-1vgusud-6"
+})(["display:grid;grid-gap:16px;grid-template-columns:repeat(auto-fill,minmax(266px,1fr));"]);
+var TopText = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].div.withConfig({
+  displayName: "Home__TopText",
+  componentId: "sc-1vgusud-7"
+})(["font-size:60px;margin-bottom:24px;"]);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.c2861f33e725fca70885.hot-update.js.map
